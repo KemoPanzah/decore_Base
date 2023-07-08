3ef010ab70276950dcbb84859eff912014a7e405
Kemo (decon-pc-71) <panzah.one@googlemail.com>
2023-07-08 12:06:05 +0200

    test: test passwordfield

--- decore_test/test_fields.py
+++ decore_test/test_fields.py
@@ -11,11 +11,20 @@ class Test_model(Conform_model):
 # Testklasse für MyClass
 class Test_fields:
     def setup_item(self):
+        Test_model.create_table()
         self.test_item = Test_model()
         self.test_item.id = 'bcc2c3a4-1d52-11ee-9d5f-309c23812330'
         self.test_item.title = "Test Item"
 
-    def test_set_password_field(self):
+    def test_password_field(self):
         self.setup_item()
+        self.test_item.password = "12345678"
+        self.test_item.save()
+        
+        assert self.test_item.password == '12345678'
+        assert self.test_item.__data__['password'] != '12345678'
 
-        assert self.test_item.password == None
+        db_item = Test_model.get(Test_model.id == self.test_item.id)
+
+        assert db_item.password == "12345678"
+
